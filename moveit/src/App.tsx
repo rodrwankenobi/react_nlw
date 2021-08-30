22b2d8620e50030f92ef3c8cae9e78c9426521eb
rodrwankenobi <49798159+rodrwankenobi@users.noreply.github.com>
2021-08-30 01:04:48 -0300

    dia 1

---
+++ moveit/src/App.tsx
@@ -0,0 +1,14 @@
+import { ExperienceBar } from './components/ExperienceBar';
+import React from 'react';
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+
+  );
+}
+
+export default App;
